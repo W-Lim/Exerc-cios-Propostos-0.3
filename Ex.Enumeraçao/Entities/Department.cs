4b1bec31b4f00484dc71159745a41d89219146fc
Wender lima <wenderlima678@gmail.com>
2021-07-16 18:02:40 -0300

    Exercicio completo, Enumeraçao

---
+++ Ex.Enumeraçao/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex.Enumeraçao.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department (string name)
+        {
+            Name = name;
+        }
+    }
+
+}
